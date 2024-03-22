cbab5ba3d584bbef4e810ee379f0ee7d7a74970b
Gwakisa Mwangomo <87722087+gmwangomo@users.noreply.github.com>
2024-03-22 08:43:33 +0300

    Update database.py
    Update setting

--- database.py
+++ database.py
@@ -4,6 +4,6 @@ def get_database_connection():
     return mysql.connector.connect(
         host="localhost",
         user = "root",
-        password="Mwangomo#1",
+        password="",
         database="fastApiShule"
     )
